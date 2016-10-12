8d1dec01259cc3ead446a498817afcfb98d100a6
shan8017 <shan8017@gmail.com>
2016-10-12 18:18:46 +0900

    view modify to remove timezone filter

--- blog2/views.py
+++ blog2/views.py
@@ -3,9 +3,9 @@ from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 # Create your views here.
-
+#filter(created_date__lte=timezone.now())
 def post_list(request):
-    posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    posts = Post.objects.all().order_by('created_date')
     return render(request, 'blog2/post_list.html',{'posts': posts})
 
 def post_detail(request, pk):
